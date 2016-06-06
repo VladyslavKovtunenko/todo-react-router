fa1dd09b56a1e6a927c40c6189494690b313c4df
VladyslavKovtunenko <kovtunenkovladyslav@gmail.com>
2016-06-06 19:45:57 +0300

    reducer

--- configureStore.js
+++ configureStore.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
-import {todo} from "./redusers/requestReduser";
+import {todo} from "reducers/requestReducer";
 
 // const reducer = combineReducers(reducers);
 const createStoreWithMiddleware = applyMiddleware(apiMiddleware)(createStore);
